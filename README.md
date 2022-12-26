02f6de6598bbf3ad1ddb6f0f74f9b04f172767a4
Maurits van Altvorst <mvanaltvorst@icloud.com>
2022-12-26 09:10:40 +0000

    document

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Pareto mixture
 
+### Installation
+```
+cd pareto_mixture/
+conda activate <YOUR_CONDA_ENVIRONMENT>
+conda install pip
+python -m pip install -e .
+```
+
+### Usage
+In `research/how_to_use.ipynb`, you will find a notebook that shows how to use the package and highlights current issues.
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='pyparetomixture',
     version='0.1.0',
-    description='This library fits a pareto mixture on data using L-BFGS to solve max likelihood.',
+    description="This library fits a pareto mixture on data using Newton's method to solve max likelihood.",
     author="Maurits van Altvorst",
     author_email="mvanaltvorst@icloud.com",
     packages=["pyparetomixture"],

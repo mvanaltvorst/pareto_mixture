ff11ebecc14a5e92f37697130ec60f34efa5aa1b
Maurits van Altvorst <mvanaltvorst@icloud.com>
2023-04-05 21:40:28 +0000

    do directed search

--- src/pyparetomixture/type.py
+++ src/pyparetomixture/type.py
@@ -8,6 +8,34 @@ class ParetoMixtureParameters:
     beta: float
     p: float
 
+    def __add__(self, other):
+        return ParetoMixtureParameters(
+            self.alpha + other.dalpha,
+            self.beta + other.dbeta,
+            self.p + other.dp,
+        )
+
+@dataclass
+class DeltaParetoMixtureParameters:
+    dalpha: float
+    dbeta: float
+    dp: float
+    # scalar multiply
+    def __mul__(self, other):
+        return DeltaParetoMixtureParameters(
+            self.dalpha * other,
+            self.dbeta * other,
+            self.dp * other,
+        )
+
+    # add to ParetoMixtureParameters
+    def __add__(self, other):
+        return ParetoMixtureParameters(
+            self.dalpha + other.alpha,
+            self.dbeta + other.beta,
+            self.dp + other.p,
+        )
+
 
 @dataclass
 class Sample:
